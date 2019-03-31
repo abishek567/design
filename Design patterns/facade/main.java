c19cdade3705f80a98829e54c13de7bcd09aa620
abishek567 <48051148+abishek567@users.noreply.github.com>
2019-03-31 22:40:16 +0530

    Update main.java

--- Design patterns/facade/main.java
+++ Design patterns/facade/main.java
@@ -1,11 +1,12 @@
 package com.design.facade;
 
+
 public class Main {
 
 	public static void main(String[] args) 
-	{
-        BankAccount account = new BankAccount(12345, 1234);  
-        
-        account.withdrawlCash(50.00);
+	{ 
+		AccountManager acnt = new AccountManager();   
     }
 }
+
+
